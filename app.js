1dff4f55eeee20ef2792f5589292b371da8a5ffe
benconsult <bendayo38@gmail.com>
2024-04-24 03:45:58 +0100

    setup front end

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
+const path = require('path')
+
 //extra security packages
 const helmet = require('helmet')
 const xss = require('xss-clean')
@@ -21,7 +23,8 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
+//static files middleware
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -35,6 +38,11 @@ app.use(xss());
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs', authenticatedUser, jobsRouter)
+
+//to use indext.html of the react app
+app.get('*', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
